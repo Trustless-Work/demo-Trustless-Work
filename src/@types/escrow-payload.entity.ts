2b4430fee143d4352346d8c34231668fd726f27b
Joel Vargas <vermudo.com@gmail.com>
2025-05-01 16:42:12 -0600

    feat: add GetEscrowPayload type and refactor EditMilestonesPayload to EditEscrowPayload, enhancing type definitions for escrow operations

--- src/@types/escrow-payload.entity.ts
+++ src/@types/escrow-payload.entity.ts
@@ -44,12 +44,16 @@ export type FundEscrowPayload = Pick<Escrow, "amount" | "contractId"> & {
   signer: string;
 };
 
+export type GetEscrowPayload = Pick<Escrow, "contractId"> & {
+  signer: string;
+};
+
 export type DistributeEscrowEarningsEscrowPayload = Pick<Escrow, "contractId"> &
   Partial<Pick<Escrow, "serviceProvider" | "releaseSigner">> & {
     signer: string;
   };
 
-export type EditMilestonesPayload = {
+export type EditEscrowPayload = {
   contractId: string;
   escrow: EscrowPayload;
   signer: string;
@@ -59,3 +63,14 @@ export type GetBalanceParams = {
   signer: string;
   addresses: string[];
 };
+
+export type EscrowPayloadService =
+  | InitializeEscrowPayload
+  | GetEscrowPayload
+  | ChangeMilestoneStatusPayload
+  | ChangeMilestoneFlagPayload
+  | StartDisputePayload
+  | ResolveDisputePayload
+  | FundEscrowPayload
+  | DistributeEscrowEarningsEscrowPayload
+  | EditEscrowPayload;
